961a1e542627e5d9826a1a3ffbdc4de176d6cde2
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-02-23 16:29:42 +0300

    refactor: change the decision transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,26 +3,25 @@ module.exports = function transform(arr) {
         throw Error;
     } 
     let mas = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== '--discard-prev' || arr[i] !== '--discard-next' || arr[i] !== '--double-next' || arr[i] !== '--double-prev') {
-            mas.push(arr[i]);
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] !== '--discard-prev' && arr[i] !== '--discard-next' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+                mas.push(arr[i]);
+            }
+            else if (arr[i] === '--discard-prev' && i != 0) {
+                mas.pop();
+            } else if (arr[i] === '--discard-next' && i != arr.length - 1) {
+                arr.splice(i, 1);
+            } else if (arr[i] === '--double-next' && i != arr.length - 1) {
+                arr[i] = arr[i + 1];
+                mas.push(arr[i]);
+            } else if (arr[i] === '--double-prev' && i != 0) {
+                mas.push(arr[i-1]);
+            } else if (arr[i] === '--double-prev' && i == 0 || arr[i] === '--discard-prev' && i == 0) {
+            mas.pop();
+            } else if (arr[arr.length - 1] === '--double-next' || arr[arr.length - 1] === '--discard-next') {
+            arr.splice(arr.length - 1, 0);
+            }
         }
-        else if (arr[i] === '--discard-prev' && i != 0) {
-            arr.splice(indexOf(arr[i - 1]), 2);
-        } else if (arr[i] === '--discard-next' && i != arr.length - 1) {
-            arr.splice(indexOf(arr[i]), 2);
-        } else if (arr[i] === '--double-next' && i != arr.length - 1) {
-            arr[i] = arr[i + 1];
-            mas.push(arr[i]);
-        } else if (arr[i] === '--double-prev' && i != 0) {
-            arr[i] = arr[i - 1];
-            mas.push(arr[i]);
-    } else if (arr[0] === '--double-prev' || arr[0] === '--discard-prev') {
-        arr.splice(0,1);
-
-    } else if (arr[arr.length - 1] === '--double-next' || arr[arr.length - 1] === '--discard-next') {
-        arr.splice(arr[arr.length - 1], 1);
-    }
-}
-return mas;
+    return mas;
 };
+
