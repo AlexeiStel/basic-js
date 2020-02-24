4258836073dd4c58e285cd7c0b15b95561693584
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-02-24 20:40:31 +0300

    feat: add the decision recursive-depth

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,7 +1,10 @@
 class VigenereCipheringMachine {
     encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+        let alphabet = ['a', 'b', 'c', 'd',  'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        let word;
+        let key;
+        let cipher;
+       
     }
 
     decrypt() {
